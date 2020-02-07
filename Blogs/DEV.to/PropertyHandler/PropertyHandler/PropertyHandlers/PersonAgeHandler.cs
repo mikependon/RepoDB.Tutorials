f63a0311f80d15d75762a5127778620d4f068236
Michael Pendon <michael.c.pendon@outlook.com>
2020-02-07 08:43:06 +0100

    Updated to the latest version of RepoDb v1.10.6.

--- Blogs/DEV.to/PropertyHandler/PropertyHandler/PropertyHandlers/PersonAgeHandler.cs
+++ Blogs/DEV.to/PropertyHandler/PropertyHandler/PropertyHandlers/PersonAgeHandler.cs
@@ -1,15 +1,18 @@
-﻿using RepoDb.Interfaces;
+﻿using RepoDb;
+using RepoDb.Interfaces;
 
 namespace PropertyHandler.PropertyHandlers
 {
     public class PersonAgeHandler : IPropertyHandler<int, string>
     {
-        public string Get(int input)
+        public string Get(int input,
+            ClassProperty property)
         {
             return $"{input} years old";
         }
 
-        public int Set(string input)
+        public int Set(string input,
+            ClassProperty property)
         {
             return int.Parse(input.Replace(" years old", string.Empty));
         }
--- Blogs/DEV.to/PropertyHandler/PropertyHandler/PropertyHandlers/PersonExtendedInfoHandler.cs
+++ Blogs/DEV.to/PropertyHandler/PropertyHandler/PropertyHandlers/PersonExtendedInfoHandler.cs
@@ -1,17 +1,20 @@
 ﻿using Newtonsoft.Json;
 using PropertyHandler.Models;
+using RepoDb;
 using RepoDb.Interfaces;
 
 namespace PropertyHandler.PropertyHandlers
 {
     public class PersonExtendedInfoHandler : IPropertyHandler<string, PersonExtendedInfo>
     {
-        public PersonExtendedInfo Get(string input)
+        public PersonExtendedInfo Get(string input,
+            ClassProperty property)
         {
             return JsonConvert.DeserializeObject<PersonExtendedInfo>(input);
         }
 
-        public string Set(PersonExtendedInfo input)
+        public string Set(PersonExtendedInfo input,
+            ClassProperty property)
         {
             return JsonConvert.SerializeObject(input);
         }
