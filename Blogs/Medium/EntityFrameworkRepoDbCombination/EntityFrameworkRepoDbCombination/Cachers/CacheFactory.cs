0e8fb1dafaf4387fe99da81b11f2411dd39e383e
mikependon <michael.c.pendon@outlook.com>
2020-07-10 16:54:48 +0200

    Committing the latest code changes.

---
+++ Blogs/Medium/EntityFrameworkRepoDbCombination/EntityFrameworkRepoDbCombination/Cachers/CacheFactory.cs
@@ -0,0 +1,28 @@
+﻿using RepoDb;
+using RepoDb.Interfaces;
+
+namespace EntityFrameworkRepoDbCombination.Cachers
+{
+    public class CacheFactory
+    {
+        private static ICache _cache;
+        private static object _syncLock;
+
+        static CacheFactory()
+        {
+            _syncLock = new object();
+        }
+
+        public static ICache Create()
+        {
+            if (_cache==null)
+            {
+                lock (_syncLock)
+                {
+                    _cache = new MemoryCache();
+                }
+            }
+            return _cache;
+        }
+    }
+}
