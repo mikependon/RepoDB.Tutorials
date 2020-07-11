d54eefeb1e33bb4addae66b9dc12adcea006e215
Michael Pendon <michael.c.pendon@outlook.com>
2020-07-11 15:54:07 +0200

    Added a pre-selection functionality.

--- WPF/TreeViewFileExplorer/TreeViewFileExplorer/MainWindow.xaml.cs
+++ WPF/TreeViewFileExplorer/TreeViewFileExplorer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -17,17 +18,7 @@ namespace TreeViewFileExplorer
             InitializeFileSystemObjects();
         }
 
-        private void InitializeFileSystemObjects()
-        {
-            var drives = DriveInfo.GetDrives();
-            DriveInfo.GetDrives().ToList().ForEach(drive =>
-            {
-                var fileSystemObject = new FileSystemObjectInfo(drive);
-                fileSystemObject.BeforeExplore += FileSystemObject_BeforeExplore;
-                fileSystemObject.AfterExplore += FileSystemObject_AfterExplore;
-                treeView.Items.Add(fileSystemObject);
-            });
-        }
+        #region Events
 
         private void FileSystemObject_AfterExplore(object sender, System.EventArgs e)
         {
@@ -38,5 +29,91 @@ namespace TreeViewFileExplorer
         {
             Cursor = Cursors.Wait;
         }
+
+        #endregion
+
+        #region Methods
+
+        private void InitializeFileSystemObjects()
+        {
+            var drives = DriveInfo.GetDrives();
+            DriveInfo
+                .GetDrives()
+                .ToList()
+                .ForEach(drive =>
+                {
+                    var fileSystemObject = new FileSystemObjectInfo(drive);
+                    fileSystemObject.BeforeExplore += FileSystemObject_BeforeExplore;
+                    fileSystemObject.AfterExplore += FileSystemObject_AfterExplore;
+                    treeView.Items.Add(fileSystemObject);
+                });
+            PreSelect(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+
+        private void PreSelect(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+            var driveFileSystemObjectInfo = GetDriveFileSystemObjectInfo(path);
+            driveFileSystemObjectInfo.IsExpanded = true;
+            PreSelect(driveFileSystemObjectInfo, path);
+        }
+
+        private void PreSelect(FileSystemObjectInfo fileSystemObjectInfo,
+            string path)
+        {
+            foreach (var childFileSystemObjectInfo in fileSystemObjectInfo.Children)
+            {
+                var isParentPath = IsParentPath(path, childFileSystemObjectInfo.FileSystemInfo.FullName);
+                if (isParentPath)
+                {
+                    if (string.Equals(childFileSystemObjectInfo.FileSystemInfo.FullName, path))
+                    {
+                        /* We found the item for pre-selection */
+                    }
+                    else
+                    {
+                        childFileSystemObjectInfo.IsExpanded = true;
+                        PreSelect(childFileSystemObjectInfo, path);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private FileSystemObjectInfo GetDriveFileSystemObjectInfo(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            var drive = DriveInfo
+                .GetDrives()
+                .Where(d => d.RootDirectory.FullName == directory.Root.FullName)
+                .FirstOrDefault();
+            return GetDriveFileSystemObjectInfo(drive);
+        }
+
+        private FileSystemObjectInfo GetDriveFileSystemObjectInfo(DriveInfo drive)
+        {
+            foreach (var fso in treeView.Items.OfType<FileSystemObjectInfo>())
+            {
+                if (fso.FileSystemInfo.FullName == drive.RootDirectory.FullName)
+                {
+                    return fso;
+                }
+            }
+            return null;
+        }
+
+        private bool IsParentPath(string path,
+            string targetPath)
+        {
+            return path.StartsWith(targetPath);
+        }
+
+        #endregion
     }
 }
